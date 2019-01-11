0d6c44d3d1068cc554a400cf95bae8dc8e592989
Hoang <tienhoangkhmt@gmail>
2019-01-11 15:08:46 +0700

    dsasd

--- btdiemA/HOANG.php
+++ btdiemA/HOANG.php
@@ -23,7 +23,7 @@ require 'database.php';
     <input type="text" name="matour" placeholder="nhap ma tour vao day"><br>
     <label>Ten tour</label><br>
     <input type="text" name="tour" placeholder="nhap tour can them vao day"><br>
-    <label>Gia</label><br>
+    <label>Gia</label>
     <input type="number" name="gia" placeholder="nhap gia"><br>
     <label>Thoi Gian Chay</label><br>
     <input type="text" name="thoigian" placeholder="nhap thoi gian "><br>
@@ -34,7 +34,28 @@ require 'database.php';
     <input type="submit" name="btn_submit" value="submit">
 </form>
 <?php
-
+    if(isset($_REQUEST['btn_submit'])){
+        // lay du lieu tu input
+        $ma=$_REQUEST['matour'];
+        $tour=$_REQUEST['tour'];
+        $gia=$_REQUEST['gia'];
+        $thoigian=$_REQUEST['thoigian'];
+        $songay=$_REQUEST['songay'];
+        $tinhtrang=$_REQUEST['tinhtrang'];
+        // kiem tra xem input co rong khong
+        if ($ma==""||$tour=="" || $gia==""|| $thoigian==""||$songay==""||$tinhtrang==""){
+            echo "yeu cau nhap lai";
+        }else{
+            $db=new Database();
+            $conn=Database::$connection;
+            $sql="INSERT INTO tourdulich(`matour`,`tentour`,`gia`,`thoigianchay`,`songay`,`tinhtrang`) VALUE('$ma','$tour','$gia','$thoigian','$songay','$tinhtrang') ";
+            if($conn->query($sql)== TRUE){
+                echo " them thanh cong";
+            }else{
+                echo mysqli_errno($conn) . mysqli_error($conn);
+            }
+        }
+    }
 ?>
 </body>
 </html>
